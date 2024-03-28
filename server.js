6ee51fa17a21cd37f25964c265d460c4bc2db697
Kawinda Randima <randima@thesanmark.com>
2024-03-28 18:50:05 +0530

    ejs files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+//Middleware 
+app.set('view engine', 'ejs');
+
+app.get('/' , (req, res) => {
+    res.render('index');
+});
+
+app.get('/login' , (req, res) => {
+    res.render('login');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Serverrunnign on port ${PORT}`); 
+});
